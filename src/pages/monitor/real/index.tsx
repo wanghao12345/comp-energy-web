8abd3d5b3c002e97e6a8450d1aee1eded8842380
wanghao <1750110604@qq.com>
2022-03-19 17:34:44 +0800

    update

--- src/pages/monitor/real/index.tsx
+++ src/pages/monitor/real/index.tsx
@@ -1,4 +1,8 @@
+import { Select, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import { RealContainer, RealOptionContainer, RealBodyContainer } from './style';
+
+const { Option } = Select;
 const RealPage = () => {
   return (
     <RealContainer>
@@ -9,7 +13,19 @@ const RealPage = () => {
 };
 
 const RealOption = () => {
-  return <RealOptionContainer></RealOptionContainer>;
+  return (
+    <RealOptionContainer>
+      <Select size="large" defaultValue="电" onChange={() => {}}>
+        <Option value="电">电</Option>
+        <Option value="水">水</Option>
+        <Option value="蒸汽">蒸汽</Option>
+        <Option value="空气">空气</Option>
+        <Option value="氮气">氮气</Option>
+        <Option value="天然气">天然气</Option>
+      </Select>
+      <Input size="large" suffix={<SearchOutlined />} placeholder="节点名称" />
+    </RealOptionContainer>
+  );
 };
 
 const RealBodyOption = () => {
--- src/pages/monitor/real/style.ts
+++ src/pages/monitor/real/style.ts
@@ -11,6 +11,21 @@ export const RealOptionContainer = styled.div`
   border: 1px solid rgba(67, 140, 172, 100);
   border-radius: 4px;
   margin-right: 24px;
+  box-sizing: border-box;
+  padding: 24px 16px;
+  .ant-select {
+    width: 100%;
+    margin-bottom: 12px;
+    .ant-select-selector {
+      /* background-color: rgba(52, 125, 155, 20); */
+      /* border: 1px solid rgba(67, 140, 172, 60); */
+    }
+  }
+  .ant-input {
+    border-radius: 4px;
+    /* background-color: rgba(52, 125, 155, 20);
+    border: 1px solid rgba(67, 140, 172, 60); */
+  }
 `;
 export const RealBodyContainer = styled.div`
   flex: 1;
