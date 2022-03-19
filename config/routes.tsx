c55567228a9f37873484df1cd26ca2ffc2c50b05
wanghao <1750110604@qq.com>
2022-03-19 12:59:58 +0800

    update

--- config/routes.tsx
+++ config/routes.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { LaptopOutlined } from '@ant-design/icons';
 export default [
   {
--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -1,19 +1,49 @@
 import React from 'react';
-// import { Tabs } from 'antd';
+import { Tabs } from 'antd';
 import styles from './index.less';
 
-// const { TabPane } = Tabs;
+const { TabPane } = Tabs;
 export const TabMenu: React.FC = ({ children }) => {
+  const tabs = [
+    {
+      label: 'tab 1',
+    },
+    {
+      label: 'tab 2',
+    },
+    {
+      label: 'tab 3',
+    },
+    {
+      label: 'tab 4',
+    },
+    {
+      label: 'tab 5',
+    },
+    {
+      label: 'tab 6',
+    },
+    {
+      label: 'tab 7',
+    },
+  ];
   return (
-    <div className={styles.tabMenuContainer}>
-      <div className={styles.tabMenuHeader}>
-        {/* <Tabs defaultActiveKey="1" tabPosition={'top'} style={{ height: 220 }}>
-          {[...Array.from({ length: 30 }, (v, i) => i)].map(i => (
-            <TabPane tab={`Tab-${i}`} key={i} disabled={i === 28}>
-              Content of tab {i}
-            </TabPane>
+    <div className={styles['tabMenuContainer']}>
+      <div className={styles['tabMenuHeader']}>
+        <Tabs
+          className={styles['tabsBox']}
+          defaultActiveKey="0"
+          type="card"
+          tabPosition={'top'}
+        >
+          {tabs.map((tab: any, index: number) => (
+            <TabPane
+              className={styles['tabBox']}
+              tab={tab.label}
+              key={index + ''}
+            ></TabPane>
           ))}
-        </Tabs> */}
+        </Tabs>
       </div>
       <div className={styles.tabMenuBody}>{children}</div>
     </div>
